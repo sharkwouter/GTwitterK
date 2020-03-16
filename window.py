db40644509148d7e8dc7fb89ae8a206f16dea605
Wouter Wijsman <wwijsman@live.nl>
2020-03-17 00:30:47 +0100

    Always launch regardless of directory
    Some comments were added as well for readability and a method was
    renamed which was duplicate

--- window.py
+++ window.py
@@ -6,14 +6,18 @@ from gi.repository import Gtk, WebKit2
 
 @Gtk.Template.from_file("window.ui")
 class Window(Gtk.ApplicationWindow):
+    # Load the Window class from the ui file
     __gtype_name__ = "Window"
 
+    # Load widgets from ui file
     button_back = Gtk.Template.Child()
     button_forward = Gtk.Template.Child()
     webview_container = Gtk.Template.Child()
 
     def __init__(self, name="GTwitterK"):
         Gtk.ApplicationWindow.__init__(self, title=name)
+
+        # Create webview which will show twitter
         context = WebKit2.WebContext.new()
         self.webview = WebKit2.WebView.new_with_context(context)
 
@@ -35,5 +39,6 @@ class Window(Gtk.ApplicationWindow):
         self.webview.go_forward()
 
     @Gtk.Template.Callback("on_button_compose_clicked")
-    def forward(self, widget):
+    def compose(self, widget):
         self.webview.load_uri("https://mobile.twitter.com/compose/tweet")
+
